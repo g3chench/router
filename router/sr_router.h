b8040a6cddbb74eed7bc19cef00a51ae7e78582e
cchens <mail@christinachen.ca>
2015-10-30 03:24:27 -0400

    Cleaned up sr_router.h

--- router/sr_router.h
+++ router/sr_router.h
@@ -60,20 +60,21 @@ struct sr_instance
 int sr_verify_routing_table(struct sr_instance* sr);
 
 /* -- sr_vns_comm.c -- */
-int sr_send_packet(struct sr_instance* , uint8_t* , unsigned int , const char*);
-int sr_connect_to_server(struct sr_instance* ,unsigned short , char* );
-int sr_read_from_server(struct sr_instance* );
+int sr_send_packet(struct sr_instance* sr, uint8_t* buf, unsigned int len, const char* iface);
+int sr_connect_to_server(struct sr_instance* sr,unsigned short port, char* server);
+int sr_read_from_server(struct sr_instance* sr);
 
 /* -- sr_router.c -- */
 void sr_init(struct sr_instance* );
-void sr_handlepacket(struct sr_instance* , uint8_t * , unsigned int , char* );
+void sr_handlepacket(struct sr_instance* sr, uint8_t * packet, unsigned int len, char* interface);
+void sr_arp_handler(struct sr_instance* sr, uint8_t * packet, unsigned int len, char* interface, unsigned int minLen, uint16_t frame)
 void handle_arp_request(struct sr_instance* sr, uint8_t * packet, unsigned int len, char* interface, struct sr_if* sr_interface, sr_arp_hdr_t *arpHeader, sr_ethernet_hdr_t *etherHeader);
-void handle_arp_reply(struct sr_instance* sr, struct sr_arpreq* arpReq)
+void handle_arp_reply(struct sr_instance* sr, struct sr_arpreq* arpReq);
 
 /* -- sr_if.c -- */
-void sr_add_interface(struct sr_instance* , const char* );
-void sr_set_ether_ip(struct sr_instance* , uint32_t );
-void sr_set_ether_addr(struct sr_instance* , const unsigned char* );
-void sr_print_if_list(struct sr_instance* );
+void sr_add_interface(struct sr_instance* sr, const char* name);
+void sr_set_ether_ip(struct sr_instance* sr, uint32_t ip_nbo);
+void sr_set_ether_addr(struct sr_instance* sr, const unsigned char* addr);
+void sr_print_if_list(struct sr_instance* sr);
 
 #endif /* SR_ROUTER_H */
