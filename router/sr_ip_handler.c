4dc95f8b3f54153df4721776ca69166f9fb47a3b
cchens <mail@christinachen.ca>
2015-11-04 04:58:44 -0500

    Use sr_get_interface instead

--- router/sr_ip_handler.c
+++ router/sr_ip_handler.c
@@ -17,25 +17,25 @@
  * sr_router must check if it's an IP packet
  * then call this function
  */
-struct sr_if *get_output_interface(struct sr_instance *sr, uint32_t address) {
+// struct sr_if *get_output_interface(struct sr_instance *sr, uint32_t address) {
 
-  printf("TESTING: BEFORE current_node\n");
+//   printf("TESTING: BEFORE current_node\n");
   
-  struct sr_if *current_node = sr->if_list;
+//   struct sr_if *current_node = sr->if_list;
 
-  printf("TESTING: AFTER current_node\n");
+//   printf("TESTING: AFTER current_node\n");
   
-  while (current_node) {
-    if (address == current_node->ip) {
-      printf("TESTING: FIRST return\n");
-      return current_node;
-    }
-    current_node = current_node->next;
-  }
+//   while (current_node) {
+//     if (address == current_node->ip) {
+//       printf("TESTING: FIRST return\n");
+//       return current_node;
+//     }
+//     current_node = current_node->next;
+//   }
 
-  printf("TESTING: SECOND return\n");
-  return NULL;
-}
+//   printf("TESTING: SECOND return\n");
+//   return NULL;
+// }
 
 void ip_handler(struct sr_instance* sr, 
         uint8_t* packet,
@@ -85,7 +85,7 @@ void ip_handler(struct sr_instance* sr,
   /* else this IP packet is valid: */
     
   /* Check that the ip packet is being sent to this host, sr_router */
-  struct sr_if *out_interface = get_output_interface(sr, ip_hdr->ip_dst);
+  struct sr_if *out_interface = sr_get_interface(sr, interface);
 
   if (out_interface != NULL) {
         printf("This IP packet was sent to me!\n");
