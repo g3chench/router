225dc6ee712695c5b3b45bb2d5f471dd6b311b35
cchens <mail@christinachen.ca>
2015-11-04 05:04:41 -0500

    Removed get_output_interface function

--- router/sr_ip_handler.c
+++ router/sr_ip_handler.c
@@ -17,25 +17,6 @@
  * sr_router must check if it's an IP packet
  * then call this function
  */
-// struct sr_if *get_output_interface(struct sr_instance *sr, uint32_t address) {
-
-//   printf("TESTING: BEFORE current_node\n");
-  
-//   struct sr_if *current_node = sr->if_list;
-
-//   printf("TESTING: AFTER current_node\n");
-  
-//   while (current_node) {
-//     if (address == current_node->ip) {
-//       printf("TESTING: FIRST return\n");
-//       return current_node;
-//     }
-//     current_node = current_node->next;
-//   }
-
-//   printf("TESTING: SECOND return\n");
-//   return NULL;
-// }
 
 void ip_handler(struct sr_instance* sr, 
         uint8_t* packet,
