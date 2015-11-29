bfd1072872ec035cf90b90b5adf1f3a21a131db0
cchens <mail@christinachen.ca>
2015-11-28 22:26:17 -0500

    bracket issue

--- router/sr_arp_handler.h
+++ router/sr_arp_handler.h
@@ -5,6 +5,6 @@ void arp_handler(struct sr_instance* sr, uint8_t * packet, unsigned int len, cha
 
 void handle_arp_request(struct sr_instance* sr, uint8_t * packet, unsigned int len, char* interface, struct sr_if* sr_interface, sr_arp_hdr_t *arp_hdr, sr_ethernet_hdr_t *eth_hdr);
 
-void handle_arp_reply(struct sr_instance* sr, uint8_t* packet, struct sr_arpreq* arp_req) {
+void handle_arp_reply(struct sr_instance* sr, uint8_t* packet, struct sr_arpreq* arp_req);
 
 #endif
