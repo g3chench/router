fe32cb7a1069b313cfb3db04f6e38cbfd0ed3ebc
cchens <mail@christinachen.ca>
2015-12-11 02:33:07 -0500

    changed param to sr_nat_timeout

--- router/sr_nat.c
+++ router/sr_nat.c
@@ -51,10 +51,10 @@ int sr_nat_destroy(struct sr_nat *nat) {  /* Destroys the nat (free memory) */
 
 }
 
-void *sr_nat_timeout(void *sr_ptr) {  /* Periodic Timout handling */
+void *sr_nat_timeout(void *nat_ptr) {  /* Periodic Timout handling */
 
-  struct sr_instance *sr = (struct sr_instance *)sr_ptr;
-  struct sr_nat *nat = sr->nat;
+  struct sr_nat *nat = (struct sr_nat *)nat_ptr;
+  struct sr_instance *sr = nat->sr;
   
   while (1) {
     sleep(1.0);
